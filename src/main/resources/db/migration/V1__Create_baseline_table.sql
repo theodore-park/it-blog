f12552b9407b1a79d28517be5111aef2ab1fac48
Theodore Park <pky901107@naver.com>
2019-12-24 22:49:23 +0900

    flyway setting complete.

--- src/main/resources/db/migration/V1__Create_baseline_table.sql
+++ src/main/resources/db/migration/V1__Create_baseline_table.sql
@@ -1,5 +1,6 @@
-CREATE TABLE `USERS`
+CREATE TABLE `users`
 (
 `id`bigint(20) NOT NULL AUTO_INCREMENT,
-`name` varchar(30) NOT NULL
-)
+`name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
